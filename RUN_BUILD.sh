f49f32076367ce9f471eb9a513e1359443cb34b8
Denis Belov <dsbelov63@gmail.com>
2021-07-12 07:50:13 -0400

    up

---
+++ RUN_BUILD.sh
@@ -0,0 +1,1 @@
+/home/denis/.nvm/versions/node/v14.4.0/bin/node $(pwd)/node_modules/webpack/bin/webpack.js --env=development
---
+++ RUN_START.sh
@@ -0,0 +1,1 @@
+/home/denis/.nvm/versions/node/v14.4.0/bin/node $(pwd)/node_modules/webpack-dev-server/bin/webpack-dev-server.js --env=development --open
